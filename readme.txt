47043000d20ed6044dc4f060c1f26651d84412d0
Jeff Stieler <jeff.m.stieler@gmail.com>
2012-10-19 09:41:56 -0400

    Initial Commit.

---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== Voce Submenu Items ===
+Contributors: jeffstieler
+Tags: menus, navigation, nested
+Requires at least: 3.4.2
+Tested up to: 3.4.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Nest menus as items to avoid giant navigation menus in your admin.
+
+== Description ==
+
+This plugin adds Menus themselves as an item you can put in other Menus as placeholders representing their contents.
+
+When a Menu containing other Menus as items is rendered, the "submenu" item is expanded out into all of its items.
+
+== Installation ==
+
+1. Upload `voce-submenu-items` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+1. Menus admin screen showing "Navigation Menus" metabox and a menu containing another menu as an item.
+1. Menus admin screen showing the contents of the "sub" menu.
+1. The resulting menu.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release.
---
+++ voce-submenu-items.php
@@ -0,0 +1,95 @@
+<?php
+/*
+Plugin Name: Voce Submenu Nav Menu Items
+Plugin URI: http://voceconnect.com
+Description: Avoid giant Nav Menus by aggregating multiple "submenus" into a single Menu.
+Version: 1.0
+Author: Jeff Stieler
+License: GPL2
+*/
+
+class Voce_Submenu_Nav_Menu_Items {
+
+	/**
+	 * Attach nav menu page hook to add metabox and nav menu object filter to expand submenu items
+	 */
+	function init() {
+
+		add_action( 'admin_head-nav-menus.php', array( __CLASS__, 'add_nav_menu_metabox' ) );
+		add_filter( 'wp_nav_menu_objects', array( __CLASS__, 'expand_submenu_items' ) );
+
+	}
+
+	/**
+	 * Adds a "Navigation Menus" metabox to the Appearance > Menus page
+	 */
+	function add_nav_menu_metabox() {
+
+		$tax = get_taxonomy( 'nav_menu' );
+		$id  = $tax->name;
+
+		// shamelessly stolen from wp_nav_menu_taxonomy_meta_boxes()
+		add_meta_box( "add-{$id}", $tax->labels->name, array( __CLASS__, 'nav_menu_metabox' ), 'nav-menus', 'side', 'default', $tax );
+
+	}
+
+	/**
+	 * Callback for the Menus page metabox
+	 * Adds a filter to 'get_term_args' to exclude the current menu
+	 *
+	 * @param object $object
+	 * @param string $taxonomy
+	 */
+	function nav_menu_metabox( $object, $taxonomy ) {
+
+		add_filter( 'get_terms_args', array( __CLASS__, 'exclude_current_menu_from_metabox' ) );
+
+		wp_nav_menu_item_taxonomy_meta_box( $object, $taxonomy );
+
+		remove_filter( 'get_terms_args', array( __CLASS__, 'exclude_current_menu_from_metabox' ) );
+
+	}
+
+	/**
+	 * Exclude the menu currently being edited from the query for the Menus page metabox
+	 *
+	 * @param array $args
+	 * @return array
+	 */
+	function exclude_current_menu_from_metabox( $args ) {
+		global $nav_menu_selected_id;
+
+		if ( $nav_menu_selected_id ) {
+			$args['exclude'] = array( $nav_menu_selected_id );
+		}
+
+		return $args;
+	}
+
+	/**
+	 * Filters 'wp_nav_menu_objects', replaces items of type "nav_menu" with the items from that Nav Menu object
+	 *
+	 * @param array $sorted_menu_items
+	 * @return array
+	 */
+	function expand_submenu_items( $sorted_menu_items ) {
+
+		foreach ( $sorted_menu_items as $i => $menu_item ) {
+
+			if ( ( 'taxonomy' === $menu_item->type ) &&
+				 ( 'nav_menu' === $menu_item->object ) &&
+				 ( $submenu_items = wp_get_nav_menu_items( $menu_item->object_id ) ) ) {
+
+				array_splice( $sorted_menu_items, ( $i - 1 ), 1, $submenu_items );
+
+			}
+
+		}
+
+		return $sorted_menu_items;
+
+	}
+
+}
+
+add_action( 'init', array( 'Voce_Submenu_Nav_Menu_Items', 'init' ) );
