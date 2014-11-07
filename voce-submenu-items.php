63986f8de932c75dcaff45db6ccb9a1f4cc9c9f9
Curtis Loisel <curtis@voceconnect.com>
2014-11-07 13:59:08 -0500

    Removing unnecessary index arg

--- voce-submenu-items.php
+++ voce-submenu-items.php
@@ -84,9 +84,9 @@ class Voce_Submenu_Nav_Menu_Items {
 
 				// If item has a parent, Give the ID to all items being spliced
 				if ( $parent_id = $menu_item->menu_item_parent ) {
-					foreach ( $submenu_items as $k => $item ) {
+					foreach ( $submenu_items as &$item ) {
 						if ( empty($item->menu_item_parent) )
-							$submenu_items[$k]->menu_item_parent = $parent_id;
+							$item->menu_item_parent = $parent_id;
 					}
 				}
 
